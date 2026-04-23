1b5e4acfd19a49a5c1730d3d49a27144ece79c24
acoliver <acoliver@gmail.com>
2026-04-23 20:32:36 -0300

    bartranslate-aco 2.1.1 nounzip install fix

--- Formula/bartranslate-aco.rb
+++ Formula/bartranslate-aco.rb
@@ -1,14 +1,14 @@
 class BartranslateAco < Formula
   desc "macOS menu bar translation app (ACO fork)"
   homepage "https://github.com/acoliver/BarTranslate"
-  url "https://github.com/acoliver/BarTranslate/releases/download/v2.1.1/bartranslate-aco-v2.1.1-universal-apple-darwin.zip"
+  url "https://github.com/acoliver/BarTranslate/releases/download/v2.1.1/bartranslate-aco-v2.1.1-universal-apple-darwin.zip", using: :nounzip
   version "2.1.1"
   sha256 "6ab3bc4b01b46fa76723d9c7a4a11290048fc6d5c436ab5d86d0a882b53bcf37"
   license "GPL-3.0-only"
 
   def install
-    app_bundle = buildpath.basename.to_s == "BarTranslateACO.app" ? buildpath : buildpath/"BarTranslateACO.app"
-    prefix.install app_bundle => "BarTranslateACO.app"
+    system "ditto", "-x", "-k", cached_download, buildpath
+    prefix.install "BarTranslateACO.app"
     bin.install_symlink prefix/"BarTranslateACO.app/Contents/MacOS/BarTranslateACO" => "bartranslate-aco"
   end
 
